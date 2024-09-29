72dc274224ecb25edbe05f454434e243e5827122
michal.valo <mvalowork@gmail.com>
2024-09-29 17:14:45 +0100

    2/3 connectivity

--- Program.cs
+++ Program.cs
@@ -11,14 +11,26 @@ namespace ConferencingServer
     {
         public class User
         {
-            int id { get; set; }
-            string name { get; set; }
-            string ip { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string ip { get; set; }
+            public UdpClient portChatDown { get; set; }
+            public UdpClient portAudioDown { get; set; }
+            public UdpClient portVideoDown { get; set; }
+            public UdpClient portChatUp { get; set; }
+            public UdpClient portAudioUp { get; set; }
+            public UdpClient portVideoUp { get; set; }
             public User(int id, string name, string ip)
             {
                 this.id = id;
                 this.name = name;
                 this.ip = ip;
+                this.portChatDown = new UdpClient(5001+6*id);
+                this.portAudioDown = new UdpClient(5002+6*id);
+                this.portVideoDown = new UdpClient(5003+6*id);
+                this.portChatUp = new UdpClient(5004+6*id);
+                this.portAudioUp = new UdpClient(5005+6*id);
+                this.portVideoUp = new UdpClient(5006+6*id);
             }
         }
         private static UdpClient comms = new UdpClient(5000);
@@ -45,11 +57,19 @@ namespace ConferencingServer
                                 users.Add(new User(users.Count, input[1], client.Address.ToString()));
                                 //add new upstream/downstream channels
                                 break;
+                            case "changeName":
+                                foreach (User user in users)
+                                {
+                                    if (user.ip.Equals(client.Address.ToString()))
+                                    {
+                                        user.name = input[1];
+                                    }
+                                }
+                                break;
                             default:
+                                Debug.WriteLine("Unknown command: "+input[0]);
                                 break;
                         }
-                        users.Add(client.Address.ToString());
-                        users.
                         //string sendText = string.Join(Environment.NewLine, output);
                         //connect.Connect(users[users.Count - 1], 5001 + users.Count * 3);
                         string sendThis = (5001 + users.Count * 3).ToString();
