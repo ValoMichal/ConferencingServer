52769a514acc3ec2bf0c00a1eb9579161e35cab4
michal.valo <mvalowork@gmail.com>
2024-09-29 16:52:11 +0100

    added connectivity (most beatiful lines of code ever)

--- Program.cs
+++ Program.cs
@@ -9,10 +9,21 @@ namespace ConferencingServer
 {
     internal class Program
     {
-        UdpClient comms = new UdpClient(5000);
+        public class User
+        {
+            int id { get; set; }
+            string name { get; set; }
+            string ip { get; set; }
+            public User(int id, string name, string ip)
+            {
+                this.id = id;
+                this.name = name;
+                this.ip = ip;
+            }
+        }
+        private static UdpClient comms = new UdpClient(5000);
         private static IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);
-        private static Dictionary<int,string> users = new Dictionary<int,string>();
-        private static Dictionary<int, string> names = new Dictionary<int, string>();
+        private static List<User> users = new List<User>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -26,9 +37,20 @@ namespace ConferencingServer
                 {
                     try
                     {
-                        byte[] data = connect.Receive(ref peer);
-                        string dataText = Encoding.ASCII.GetString(data);
-                        users.Add(peer.Address.ToString());
+                        List<string> input = new List<string>(Encoding.ASCII.GetString(comms.Receive(ref client)).Split(new[] { Environment.NewLine }, StringSplitOptions.None));
+                        switch (input[0])
+                        {
+                            case "connect":
+                                comms.Send(Encoding.ASCII.GetBytes(users.Count.ToString()),users.Count.ToString().Length,client.Address.ToString(),5000);
+                                users.Add(new User(users.Count, input[1], client.Address.ToString()));
+                                //add new upstream/downstream channels
+                                break;
+                            default:
+                                break;
+                        }
+                        users.Add(client.Address.ToString());
+                        users.
+                        //string sendText = string.Join(Environment.NewLine, output);
                         //connect.Connect(users[users.Count - 1], 5001 + users.Count * 3);
                         string sendThis = (5001 + users.Count * 3).ToString();
                         byte[] output = Encoding.ASCII.GetBytes(sendThis);
